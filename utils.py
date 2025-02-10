db2cc2c995d3c0bd717a7f06b1d50aebc2858046
danilowi <danilowi@agh.edu.pl>
2025-02-10 12:04:02 +0100

    utils added

---
+++ utils.py
@@ -0,0 +1,26 @@
+from os.path import join
+import shutil
+from pathlib import Path
+
+import glob
+import re
+import torch
+
+
+def increment_path(path, exist_ok=True, sep=''):
+    # Increment path, i.e. runs/exp --> runs/exp{sep}0, runs/exp{sep}1 etc.
+    path = Path(path)  # os-agnostic
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}{sep}*")  # similar paths
+        matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]  # indices
+        n = max(i) + 1 if i else 2  # increment number
+        return f"{path}{sep}{n}"  # update path
+    
+
+def save_checkpoint(state, is_best, save_dir, filename='checkpoint.pth.tar'):
+    torch.save(state, join(save_dir, filename))
+    if is_best:
+        shutil.copyfile(join(save_dir, filename), join(save_dir, 'model_best.pth.tar'))
